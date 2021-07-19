fcae0c5cc8a92d00114ab08d397ddc6e64e30f86
จ.ส.ท.พงศธร อุ่นตาล <mossamursamur@gmail.com>
2021-07-19 22:52:56 +0700

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { longdo, map, LongdoMap } from './longdo-map/LongdoMap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  initMap(){
+    map.Layers.setBase(longdo.Layers.GRAY);
+  }
+
+  // make sure to use different map key: https://map.longdo.com/api
+  render() {
+    const mapKey = '7313fc5f380ef258448202c0029934fa'
+    return (
+      <div className="App">
+        <LongdoMap id="longdo-map" mapKey={mapKey} callback={this.initMap} />
+      </div>
+    );
+  }
 }
 
 export default App;
